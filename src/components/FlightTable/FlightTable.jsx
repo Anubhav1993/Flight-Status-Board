7250917a828181f54b6ac928ed2cb031cd1e700d
Anubhav <anubhav.chaubey1993@gmail.com>
2024-03-06 12:54:40 +0530

    Update FlightTable.jsx
    Added comments for better developer readability

--- src/components/FlightTable/FlightTable.jsx
+++ src/components/FlightTable/FlightTable.jsx
@@ -3,17 +3,18 @@ import { FLIGHT_API_URL } from "../../utils/Constants";
 import "./FlightTable.css";
 import FlightRow from "../FlightRow/FlightRow";
 const FlightTable = () => {
-  const [flights, setFlights] = useState([]);
+  const [flights, setFlights] = useState([]);//State for Flights data
   const [loading, setLoading] = useState(true); //State for Loading Indicator
-  const [error, setError] = useState(null);
+  const [error, setError] = useState(null);// State for error
 
   useEffect(() => {
     fetchFlights();
     const interval = setInterval(fetchFlights, 10000); // Fetch every 10 seconds
 
-    return () => clearInterval(interval);
+    return () => clearInterval(interval);// Clear Interval on unmounting phase
   }, []);
 
+  //Function to get the Flights Data from the API
   const fetchFlights = async () => {
     try {
       const response = await fetch(FLIGHT_API_URL);
@@ -21,11 +22,12 @@ const FlightTable = () => {
       setFlights(jsonResponse);
       setLoading(false);
     } catch (error) {
-      setError("Error fetching flights. Please try again later.");
+      setError("Error fetching flights. Please try again later.");// Setting the error if the API fails
       setLoading(false);
     }
   };
 
+  //Using Memo hook to re-render the child component only when there is a change in its props
   const memoizedFlights = useMemo(() => flights, [flights]);
   return (
     <div className="container">
